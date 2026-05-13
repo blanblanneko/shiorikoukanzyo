910fbc205be28970fe52c8e680898526f2eea2ec
blanblanneko <blanblanneko@example.com>
2026-05-13 22:33:18 +0900

    Fix: Correct Date usage in sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,14 +1,15 @@
 export default function sitemap() {
+  const lastModified = new Date().toISOString();
   return [
     {
       url: 'https://shiori-exchange.vercel.app',
-      lastModified: new RegExp().toISOString(),
+      lastModified,
       changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: 'https://shiori-exchange.vercel.app/post',
-      lastModified: new RegExp().toISOString(),
+      lastModified,
       changeFrequency: 'weekly',
       priority: 0.8,
     },
